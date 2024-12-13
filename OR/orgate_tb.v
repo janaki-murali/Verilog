3a33baea6c366386c6ec8d60a07d0c18f95fb98e
Janaki Pillai <janakipillai968300@gmail.com>
2024-12-13 17:27:15 +0530

    Create orgate_tb.v

---
+++ OR/orgate_tb.v
@@ -0,0 +1,28 @@
+module or_gate_tb;
+  reg A,B;
+  wire Y
+  or_gate o1 (.a(A),.b(B),.y(Y));
+  initial begin
+    A=0;
+    B=0;
+    #1 $display("A=%b\tB=%b\tY=%b\t",A,B,Y);
+    #10
+    A=0;
+    B=1;
+    #1 $display("A=%b\tB=%b\tY=%b\t",A,B,Y);
+    #10
+    A=1;
+    B=0;
+    #1 $display("A=%b\tB=%b\tY=%b\t",A,B,Y);
+    #10
+    A=1;
+    B=1;
+    #1 $display("A=%b\tB=%b\tY=%b\t",A,B,Y);
+    #10
+    $finish;
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+  end
+endmodule
