6ff1ffecfe515865a792dd414e429357805fc8be
Janaki Pillai <janakipillai968300@gmail.com>
2024-12-13 17:13:04 +0530

    Update fulladder_tb.v

--- Adders/fulladder_tb.v
+++ Adders/fulladder_tb.v
@@ -3,31 +3,22 @@ module full_adder_tb;
   wire S, Cout;
   full_adder a1 (A, B, Cin, S, Cout);
   initial begin
-    
     A = 1'b0; B = 1'b0; Cin = 1'b0;
     #1 $display("A=%b\tB=%b\tCin=%b\tS=%b\tCout=%b", A, B, Cin, S, Cout);
-    
     A = 1'b0; B = 1'b0; Cin = 1'b1;
     #1 $display("A=%b\tB=%b\tCin=%b\tS=%b\tCout=%b", A, B, Cin, S, Cout);
-    
     A = 1'b0; B = 1'b1; Cin = 1'b0;
     #1 $display("A=%b\tB=%b\tCin=%b\tS=%b\tCout=%b", A, B, Cin, S, Cout);
-    
     A = 1'b0; B = 1'b1; Cin = 1'b1;
     #1 $display("A=%b\tB=%b\tCin=%b\tS=%b\tCout=%b", A, B, Cin, S, Cout);
-    
     A = 1'b1; B = 1'b0; Cin = 1'b0;
     #1 $display("A=%b\tB=%b\tCin=%b\tS=%b\tCout=%b", A, B, Cin, S, Cout);
-    
     A = 1'b1; B = 1'b0; Cin = 1'b1;
     #1 $display("A=%b\tB=%b\tCin=%b\tS=%b\tCout=%b", A, B, Cin, S, Cout);
-    
     A = 1'b1; B = 1'b1; Cin = 1'b0;
     #1 $display("A=%b\tB=%b\tCin=%b\tS=%b\tCout=%b", A, B, Cin, S, Cout);
-    
     A = 1'b1; B = 1'b1; Cin = 1'b1;
     #1 $display("A=%b\tB=%b\tCin=%b\tS=%b\tCout=%b", A, B, Cin, S, Cout);
-    
     $finish;
   end
   initial begin
